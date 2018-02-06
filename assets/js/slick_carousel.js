3aecc30a01697b2e3d8c9628c439b8f8a454b9b5
Orlando <orlandodaw@gmail.com>
2018-02-06 15:33:11 -0600

    computer-solutions done

--- assets/js/slick_carousel.js
+++ assets/js/slick_carousel.js
@@ -12,20 +12,15 @@
    $('.slider-nav').slick('slickGoTo', slideno - 1);
  });
 
-var header = document.getElementById("activeLinks");
-var btns = header.getElementsByClassName("topic-paragraph");
-var rects = header.getElementsByClassName("rectangle-changing-topic");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-for (var i = 0; i < btns.length; i++) {
-  rects[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+//var header = document.getElementById("activeLinks");
+//var btns = header.getElementsByClassName("computer-solutions-topic-wrapper");
+//for (var i = 0; i < btns.length; i++) {
+//  btns[i].addEventListener("click", function() {
+//    var current = document.getElementsByClassName("active");
+//    current[0].className = current[0].className.replace(" active", "");
+//    children().className += " active";
+//  });
+//}
+$('#activeLinks .computer-solutions-topic-wrapper').on('click', function(){
+     $(this).addClass('selected').siblings().removeClass('selected');
+});
