ad73837454160b6abc6e4a2b63322804760c7853
Ramon Diaz <jdiaz4@ucol.mx>
2018-02-28 16:25:51 -0600

    blog all done

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,38 +1,13 @@
-/*$(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 555.9) {
-         $(‘blog-menu-link’).addClass(‘menu-link’)
-      }
-      if ($(this).scrollTop() < 555.9) {
-         $(‘blog-menu-link’).removeClass(‘menu-link’)
-      }
-   });
-});*/
-
 $(document).ready(function() {
-	miFuncion();
+    menuBehaviour();
 });
 
-function miFuncion() {
-  $(window).scroll(function miFuncion() {
-      if ($(this).scrollTop() > 555.9) {
-         $(‘blog-menu-link’).addClass(‘menu-link’)
-      }
-      if ($(this).scrollTop() < 555.9) {
-         $(‘blog-menu-link’).removeClass(‘menu-link’)
-      }
-   });
+function menuBehaviour() {
+  $(document).scroll(function() {
+    if ($(document).scrollTop() < ($('.background').offset().top + $('.background').innerHeight())) {
+      $('.menu-link').removeClass('alternative-link');
+    } else {
+      $('.menu-link').addClass('alternative-link');
+    }
+  });
 }
-
-/*$(document).ready(function () {
-    var scroll_pos = 0;
-    $("blog-menu-link").scroll(function () {
-        scroll_pos = $(this).scrollTop();
-        if (scroll_pos > 555.9) {
-            $("blog-menu-link").scss('color', '#07778C');
-        } else {
-            $("blog-menu-link").scss('color', '#FFFFFF');
-        }
-        console.log(scroll_pos);
-    });
-});*/
