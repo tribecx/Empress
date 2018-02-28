7f63cca4f5869ad2e8cea2853a70aec2443a0295
Orlando <orlandodaw@gmail.com>
2018-02-28 02:59:06 -0600

    contact-form done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,63 @@
- $("#msform").submit(function (event) {
-                event.preventDefault();
-                $("#submit").addClass("disabled");
-                var name = $("#name").val();
-                var email = $("#email").val();
-                var message = $("#message").val();
-                var data = "name=" + name + "&email=" + email + "&message=" + message;
-                $.ajax({
-                    type: "POST",
-                    url: "ma.php",
-                    data: data,
-                    success: function (response) {
-                        switch (response) {
-                            case "Sent":
-                                $("#form-div").html("<div class='alert alert-success' style='padding:10px 30px;'>Your message has been sent, Thank you.</div>");
-                                break;
-                            case "Failed":
-                                alert("Something went wrong, please try again.");
-                                $("#submit").removeClass("disabled");
-                                break;
-                        }
-                    }
-                });
-            });
+$(document).ready(function(){
+  $('#submit-btn').click(function(event){
+    if($("input:radio[name='radio']").is(":checked")){ 
+      event.preventDefault();
+       $.ajax({
+          dataType: 'JSON',
+          url: 'assets/php/sendmail.php',
+          type: 'POST',
+          data: $('#msform').serialize(),
+          success: function(response){
+            if(response){
+              console.log(response);
+              if(response['signal'] == 'ok'){
+               $('#msg').html('<div class="alert alert-success">'+ response['msg']+'</div>');
+              }
+              else{
+                $('#msg').html('<div class="alert alert-danger">'+ response['msg'] +'</div>');
+              }
+            }
+          },
+          error: function(){
+            $('#msg').html('<div class="alert alert-danger">Errors occur. Please try again later.</div>');
+          },
+        });
+      
+        if(animating) return false;
+        animating = true;
+
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+
+        //activate next step on progressbar using the index of next_fs
+        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+          //show the next fieldset
+          next_fs.show(); 
+          //hide the current fieldset with style
+          current_fs.animate({opacity: 0}, {
+            step: function(now, mx) {
+              //as the opacity of current_fs reduces to 0 - stored in "now"
+              //1. scale current_fs down to 80%
+              scale = 1 - (1 - now) * 0.2;
+              //2. bring next_fs from the right(50%)
+              left = (now * 50)+"%";
+              //3. increase opacity of next_fs to 1 as it moves in
+              opacity = 1 - now;
+              current_fs.css({'transform': 'scale('+scale+')'});
+              next_fs.css({'left': left, 'opacity': opacity});
+            }, 
+            duration: 800, 
+            complete: function(){
+              current_fs.hide();
+              animating = false;
+            }, 
+            //this comes from the custom easing plugin
+            easing: 'easeInOutBack'
+          });
+    }
+    else{
+      $('#alert-message-submit').html('Elige un asunto');
+    }
+  });
+});
+
--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,80 +3,70 @@ var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
-$(".next").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'transform': 'scale('+scale+')'});
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
 
-$(".previous").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
 
 $(".submit").click(function(){
 	return false;
 })
 
-$('#selection .action-button').on('click', function(){
+$('#selection .radio-custom').on('click', function(){
      $(this).addClass('active').siblings().removeClass('active');
 });
+function notempty(id){
+    // we grab the id value passed in the function
+    var value = $("#"+id).val();
+    // we get the character length
+    var len = value.length;
+    // check if the lenght is less then 1 character, you can change this value to check if less than 3 characters
+    if (len < 1){
+        return false;
+    }else{
+        return true;
+    }
+}
+ 
+$(".next").click(function(){
+    // place the returned value into a variable
+    var result = $(email).val();   
+    var caract = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/);
+    console.log(result);
+    // check if the returned value is true or false
+    if (caract.test(result) == true){
+        if(animating) return false;
+        animating = true;
+
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+
+        //activate next step on progressbar using the index of next_fs
+        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+        //show the next fieldset
+        next_fs.show(); 
+        //hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+          step: function(now, mx) {
+            //as the opacity of current_fs reduces to 0 - stored in "now"
+            //1. scale current_fs down to 80%
+            scale = 1 - (1 - now) * 0.2;
+            //2. bring next_fs from the right(50%)
+            left = (now * 50)+"%";
+            //3. increase opacity of next_fs to 1 as it moves in
+            opacity = 1 - now;
+            current_fs.css({'transform': 'scale('+scale+')'});
+            next_fs.css({'left': left, 'opacity': opacity});
+          }, 
+          duration: 800, 
+          complete: function(){
+            current_fs.hide();
+            animating = false;
+          }, 
+          //this comes from the custom easing plugin
+          easing: 'easeInOutBack'
+        });
+    }
+    else{
+     $('#alert-message').html('Correo incorrecto');
+    }
+});
+
